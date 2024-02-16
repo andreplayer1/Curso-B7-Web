d0706b281d21b7d2ad5892d1e6cd18e2124479fd
andreplayer1 <andreplayer1@gmail.com>
2024-02-16 07:07:26 -0300

    aulas1-4 B7Web
    Terminando mais algumas aulas do curso B7Web

---
+++ JavaScript/Módulo 2 Javascript Básico/aula1-4/script.js
@@ -0,0 +1,20 @@
+/*function gravidade() {
+    console.log('A gravidade do planeta é: ')
+    console.log(9.8)
+}
+
+console.log('Opa, tudo bem?')
+gravidade()*/
+
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log('Resultado: ' + resultado);
+}
+
+somar(10,15)
+
+function nomeCompleto(nome, sobrenome) {
+    console.log(`${nome} ${sobrenome}`);
+}
+
+nomeCompleto('André','Barbosa')
