61a38b1b35e2fe0af3ee6f80425c02a86dc98c5b
andreplayer1 <andreplayer1@gmail.com>
2024-02-17 05:29:15 -0300

    retorno da função

---
+++ JavaScript/Módulo 2 Javascript Básico/aula5/script.js
@@ -0,0 +1,10 @@
+function nomeCompleto(nome, sobrenome) {
+    return(`${nome} ${sobrenome}`)
+}
+
+let completo = nomeCompleto('André', 'Barbosa')
+
+console.log(completo)
+
+// Retorno da Função
+// ENTRADA>PROCESSAMENTO>SAÍDA
