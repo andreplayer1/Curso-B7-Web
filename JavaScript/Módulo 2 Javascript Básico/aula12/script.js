3cda7dba41c41a0093b42502289a08548378db72
andreplayer1 <andreplayer1@gmail.com>
2024-02-23 20:29:35 -0300

    aula12
    Funções dentro de funções

---
+++ JavaScript/Módulo 2 Javascript Básico/aula12/script.js
@@ -0,0 +1,10 @@
+function addSquares(a, b) {
+    const square = x =>{ return x * x
+    }
+
+    let sqrA = square(a);
+    let sqrB = square(b);
+    return sqrA + sqrB;
+}
+
+console.log(addSquares(2, 3));
