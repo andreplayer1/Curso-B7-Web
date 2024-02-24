f8bf4478a3fdff852ee948afc58a76b09a31d21d
andreplayer1 <andreplayer1@gmail.com>
2024-02-23 21:02:11 -0300

    aula13
    Introdução a Array

---
+++ JavaScript/Módulo 2 Javascript Básico/aula13/script.js
@@ -0,0 +1,15 @@
+let nomes = [
+    'André',
+    'Bonieky',
+    'Pedro'
+];
+
+let lista = ['alto', 30, 'blabla', true, nomes];
+
+let lista2 = ['blabla', ['xx', 'yy']];
+
+let segundo = lista2[1]
+
+console.log(lista2[1][0])
+
+console.log(segundo[0])
