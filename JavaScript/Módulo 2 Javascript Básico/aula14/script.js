432efc501ebc5eb48b2f6828f2feedf849d063f9
andreplayer1 <andreplayer1@gmail.com>
2024-02-23 23:54:47 -0300

    aula14
    Operações básicas de array

---
+++ JavaScript/Módulo 2 Javascript Básico/aula14/script.js
@@ -0,0 +1,17 @@
+let ingredientes = [
+    'agua',
+    'farinha',
+    'ovo',
+    'corante',
+    'sal'
+];
+
+ingredientes.push('cebola'); // a função push(), adiciona itens dentro do array
+
+ingredientes.pop() // a função pop() remove o ultimo item do array
+
+ingredientes.shift() // a função shift() remove o primeiro item do array
+
+console.log(ingredientes)
+
+console.log(`Total de ingredientes: ${ingredientes.length}`)
