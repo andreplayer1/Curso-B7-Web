e75043c4a91cd54bb34f33a30d211effb4a1bb86
andreplayer1 <andreplayer1@gmail.com>
2024-03-05 09:53:53 -0300

    aula7
    Revisada na aula 6 e concluí aula 7

--- JavaScript/Módulo 3 Eventros e manipulação de DOM/aula6/script.js
+++ JavaScript/Módulo 3 Eventros e manipulação de DOM/aula6/script.js
@@ -2,17 +2,20 @@ function clicou() {
     const teste = document.querySelector("#teste");
     const ul = teste.querySelector('ul');
 
+    ul.innerHTML += "<li>Item adicionado</li>"; // FORMA INCORRETA. Altera todo o html para adicionar um novo elemento. Nesse caso aqui um novo <li>.
     
-    //ul.children[0].innerHTML += "(alterado)"; //Desta forma todo o conteudo do UL é alterado.
-
-   // ul.children[0].append("(alterado)"); //Usando o append não altera todo o conteúdo. Ele simplesmente vai acrescentar o que eu pedi sem mexer em mais nada(Isso é bom porque ele não precisa repocessar todo conteúdo só para acrescentar algo.) Mas ele só funciona com texto. Se eu quiser adicionar uma "tag" um "html", não vai funcionar. Solução a seguir...
-
     let newLi = document.createElement("li");
-    newLi.innerText = "Item adicionado";
-    /* Então a forma correta de adicionar um elemento novo na tela é com o appendChild. primeiro cria-se o elemento com o document.createElement e depois adiociona ele com o appendChild 
+    newLi.innerText = ("Item adicionado"); // FORMA CORRETA. Não mexe no seu html, simplesmente adiciona o novo elemento.
 
-    Existe também o prepend que adiciona o texto no começo. O append adiciona o final
-    */
+    ul.prepend("newLi");
+ }
 
-    ul.prepend(newLi);
-}
+ /* Observações
+ 1 - O appendChild() serve para adicionar elementos.
+
+ 2 - Se for adicionar um elemento deve-se cria-lo antes de colocar como fiz no exemplo acima.
+
+ 3 - O append() serve tanto para textos como para elementos, mas se for elementos tem que ser criado antes de colocar senão entrará como texto.
+ 
+ 3 - O prepend() é a mesma coisa que o append(). A única diferença é que ele adiciona no começo.
+ */
