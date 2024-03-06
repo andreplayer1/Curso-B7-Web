d12d8378c8d6c9228116d353b0c200c8a0496274
andreplayer1 <andreplayer1@gmail.com>
2024-03-06 01:31:29 -0300

    aula9

---
+++ JavaScript/Módulo 3 Eventros e manipulação de DOM/aula9/script.js
@@ -0,0 +1,9 @@
+function clicou() {
+    const li = document.querySelector('li');
+    
+    li.style.backgroundColor = '#00f';
+    li.style.fontSize = '20px';
+    li.style.margin = '20px';
+    li.style.padding = '30px';
+    li.style.borderRadius = '10px';
+}
