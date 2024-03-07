05bdef134c3a1a60db6a8bb7f9679ca2ae127413
andreplayer1 <andreplayer1@gmail.com>
2024-03-07 09:26:49 -0300

    aula10
    Hoje aprendemos sobre classList, contains, add, remove, toggle, replace.

---
+++ JavaScript/Módulo 3 Eventros e manipulação de DOM/aula10/script.js
@@ -0,0 +1,22 @@
+function clicou() {
+    const button = document.querySelector('button');
+
+    /*
+    if (button.classList.contains('azul')) {
+        button.classList.remove('azul');
+        button.classList.add('verde');
+    } else {
+        button.classList.add('azul');
+        button.classList.remove('verde');
+    }
+    */
+
+    //button.classList.toggle('azul');
+    if (button.classList.contains('azul')) {
+        button.classList.replace('azul', 'verde');
+    } else {
+        button.classList.replace('verde', 'azul');
+    }
+}
+
+/*Hoje aprendemos sobre classList, contains, add, remove, toggle, replace */
