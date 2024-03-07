74536f6adcddd3c87ff9f673ec8e668d126b5864
andreplayer1 <andreplayer1@gmail.com>
2024-03-07 17:33:09 -0300

    aula13
    exercício de lista de tarefas

---
+++ JavaScript/Módulo 3 Eventros e manipulação de DOM/aula13/script.js
@@ -0,0 +1,16 @@
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
+
+function addItem() {
+    let newLi = document.createElement('li');
+    newLi.innerText = input.value;
+    ul.append(newLi);
+}
+
+function apertar(e) {
+    if(e.key == 'Enter') {
+        addItem();
+    }
+}
+
+input.addEventListener('keypress', apertar)
