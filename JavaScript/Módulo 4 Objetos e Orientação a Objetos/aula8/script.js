89b3e463b69c6d11270f530a2a06226f38f4011e
andreplayer1 <andreplayer1@gmail.com>
2024-03-10 04:44:28 -0300

    M4A8
    Variável/Método estático

---
+++ JavaScript/Módulo 4 Objetos e Orientação a Objetos/aula8/script.js
@@ -0,0 +1,19 @@
+class Person {
+
+    static hands = 2;
+
+    age = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    static sayHi() {
+        console.log(`Oi`)
+    }
+
+}
+
+let p1 = new Person ("Bonieky");
+
+Person.sayHi();
